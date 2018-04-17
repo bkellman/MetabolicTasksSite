d8923434a036e959a136b2eeb2d91ec943d5ad0d
bkellman <bkellman@ucsd.edu>
2018-04-16 17:37:03 -0700

    wrapper for script runs

---
+++ app/Scripts/bensRun.sh
@@ -0,0 +1,23 @@
+RUN_DIR=$1
+DATA_DIR=$2
+
+cd $RUN_DIR/workingDir
+mkdir Analysis/
+mkdir Analysis/Output/
+mkdir Analysis/Figures/
+DATA_DIR=${DATA_DIR}/*
+#docker run --rm --cpus="$NUM_CORES" -v "$RUN_DIR/workingDir":/workingdir -v "$DATA_DIR":/workingdir/Data "$IMAGE" /bin/bash -c "python -u /opt/PinAPL-Py/Scripts/PinAPL.py > /workingdir/output.log 2>&1 && chown -R www-data:www-data /workingdir"
+#matlab -nodisplay -nosplash -nodesktop -r "config_file='{$RUN_DIR}/configuration.yaml'; /opt/MetabolicTasks/run.m" > "$RUN_DIR/status.log"
+for filei in $DATA_DIR ; do
+#	filei=${RUN_DIR}/workingdir/Data/${filei}
+	echo $filei 
+	echo configuration.yaml
+	/var/www/MATLAB/R2018a/bin/matlab -softwareopengl -nodisplay -nosplash -nodesktop -r "data_file='${filei}' ;config_file='${RUN_DIR}/workingDir/configuration.yaml'; addpath(genpath('/var/www/MATLAB/lib/MetTasks/')); run" 
+done
+
+#cp output.log Analysis/Configuration/
+#cp configuration.yaml Analysis/Configuration/
+
+#zip -r ${RUN_DIR}/archive.zip ${RUN_DIR}/workingDir/Analysis/
+
+#zip -r ${RUN_DIR}/archive.zip ${RUN_DIR}/workingDir/Analysis/
