538b226b7c8af50326a026af034ee2cb5206b28f
bkellman <bkellman@ucsd.edu>
2018-04-16 17:35:26 -0700

    set standard output visuals

--- resources/views/results.blade.php
+++ resources/views/results.blade.php
@@ -1,13 +1,27 @@
 @extends('layouts.master')
+@section('content')
+  <div class="row align-justify collapse">
+    <div class="columns shrink"><h4>{{ $runName }}</h4></div>
+    <div class="columns shrink">
+      <a id="re-run" class="button warning bold" href="/rerun/{{ $hash }}" >Rerun With New Parameters</a>
+      <a id="download-archive" class="button success bold" href="/run/download/{{ $hash }}" download>Download Results Archive</a>
+    </div>
+  </div>
 
-@section('customScripts')
-    <script src="//cdnjs.cloudflare.com/ajax/libs/d3/3.5.5/d3.min.js"></script>
-    <script>
-      d3.csv("score.csv", function (myArrayOfObjects){
-        myArrayOfObjects.forEach(function (d){
-          console.log(d);
-        });
-      });
-    </script>
-@parent
+
+<div>
+  <div class="align-middle">
+    <h3>Distribution of Normalized Expression Values</h3>
+    <img src='{{"/run-images?path=".urlencode("/$hash/workingDir/Analysis/Figures/histogram.png")}}'>
+  </div>
+  <div class="align-middle">
+    <h3>Dendrogram of Samples</h3>
+    <img src='{{"/run-images?path=".urlencode("/$hash/workingDir/Analysis/Figures/score_bin_dendro.png")}}'>
+  </div>
+  <div class="align-middle">
+    <h3>Heatmap of Tasks by Sample</h3>
+    <img src='{{"/run-images?path=".urlencode("/$hash/workingDir/Analysis/Figures/score_bin_heatmap.png")}}'>
+  </div>
+</div>
+
 @stop
