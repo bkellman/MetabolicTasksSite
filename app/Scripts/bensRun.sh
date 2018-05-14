7dbb6a4c8633029f5cb3fb466db88fcf3a86e2a9
bkellman <bkellman@ucsd.edu>
2018-05-14 12:49:07 -0700

    silence filename print

--- app/Scripts/bensRun.sh
+++ app/Scripts/bensRun.sh
@@ -4,6 +4,7 @@ DATA_DIR=$2
 cd $RUN_DIR/workingDir
 mkdir Analysis/
 mkdir Analysis/Output/
+mkdir Analysis/Input/
 mkdir Analysis/Figures/
 DATA_DIR=${DATA_DIR}/*
 #docker run --rm --cpus="$NUM_CORES" -v "$RUN_DIR/workingDir":/workingdir -v "$DATA_DIR":/workingdir/Data "$IMAGE" /bin/bash -c "python -u /opt/PinAPL-Py/Scripts/PinAPL.py > /workingdir/output.log 2>&1 && chown -R www-data:www-data /workingdir"
@@ -11,8 +12,9 @@ DATA_DIR=${DATA_DIR}/*
 for filei in $DATA_DIR ; do
 #	filei=${RUN_DIR}/workingdir/Data/${filei}
 	echo $filei 
+	cp $filei Analysis/Input/
 	echo configuration.yaml
-	/var/www/MATLAB/R2018a/bin/matlab -softwareopengl -nodisplay -nosplash -nodesktop -r "data_file='${filei}' ;config_file='${RUN_DIR}/workingDir/configuration.yaml'; addpath(genpath('/var/www/MATLAB/lib/MetTasks/')); run; exit" 
+	/var/www/MATLAB/R2018a/bin/matlab -softwareopengl -nodisplay -nosplash -nodesktop -r "data_file='${filei}'; config_file='${RUN_DIR}/workingDir/configuration.yaml'; addpath(genpath('/var/www/MATLAB/lib/MetTasks/')); run; exit" 
 done
 
 #cp output.log Analysis/Configuration/
